8a5102426749f78f9e1a26651128be13c81f35ab
krongch2 <krongch2@illinois.edu>
2022-04-28 19:45:51 -0500

    add sklearn

---
+++ scripts/ex1.py
@@ -0,0 +1,60 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy import signal
+from scipy.io import wavfile
+from sklearn.decomposition import FastICA, PCA
+
+import fast_ica
+
+plt.rc('font', family='serif')
+plt.rc('text', usetex=True)
+
+def load_data():
+    N = 2000
+    time = np.linspace(0, 8, N)
+    s1 = np.sin(2*time) # sinusoidal
+    s2 = np.sign(np.sin(3*time)) # square signal
+    s3 = signal.sawtooth(2*np.pi*time) # saw tooth signal
+
+    S = np.array([s1, s2, s3])
+    A = np.array([
+        [1, 1, 1],
+        [0.5, 2, 1],
+        [1.5, 1, 2]
+        ])
+    X = A @ S
+    return X, S
+
+def plot_sources(X, S, S_predicted, S_sklearn, S_pca):
+    fig, ax = plt.subplots(nrows=2, ncols=3)
+
+    for x in X:
+        ax[0, 0].plot(x)
+        ax[0, 0].set_title('Mixture signals ($X$)')
+
+    for s in S:
+        ax[0, 1].plot(s)
+        ax[0, 1].set_title("Original sources ($S$)")
+
+    for S_predicted in S_predicted:
+        ax[1, 0].plot(S_predicted)
+        ax[1, 0].set_title("Predicted sources ($W X$) [Our FastICA]")
+
+    for S_sklearn in S_sklearn:
+        ax[1, 1].plot(S_sklearn)
+        ax[1, 1].set_title("Predicted sources ($W X$) [Sklearn FastICA]")
+
+    for S_pca in S_pca:
+        ax[1, 2].plot(S_pca)
+        ax[1, 2].set_title("Predicted sources ($W X$) [Sklearn PCA]")
+
+    fig.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    X, S = load_data()
+    S_predicted, distances = fast_ica.ica(X, cycles=100)
+    S_sklearn = FastICA(n_components=3).fit_transform(X.T).T
+    S_pca = PCA(n_components=3).fit_transform(X.T).T
+    plot_sources(X, S, S_predicted, S_sklearn, S_pca)
--- scripts/fast_ica.py
+++ scripts/fast_ica.py
@@ -1,29 +1,7 @@
-from matplotlib import pyplot as plt
 import numpy as np
 import numpy.linalg as la
-from scipy import signal
-from scipy.io import wavfile
 
 import tests
-
-plt.rc('font', family='serif')
-plt.rc('text', usetex=True)
-
-def load_data():
-    N = 2000
-    time = np.linspace(0, 8, N)
-    s1 = np.sin(2*time) # sinusoidal
-    s2 = np.sign(np.sin(3*time)) # square signal
-    s3 = signal.sawtooth(2*np.pi*time) # saw tooth signal
-
-    S = np.array([s1, s2, s3])
-    A = np.array([
-        [1, 1, 1],
-        [0.5, 2, 1],
-        [1.5, 1, 2]
-        ])
-    X = A @ S
-    return X, S
 
 def center(X, standardize=False):
     mu = X.mean(axis=1, keepdims=True)
@@ -39,6 +17,7 @@ def whiten(X, test=True):
 
     if test:
         tests.test_identity(np.cov(Z))
+
     return Z
 
 def g(x, a1=1):
@@ -47,7 +26,7 @@ def g(x, a1=1):
 def dg(x):
     return 1 - g(x)**2
 
-def ica(X, cycles=1000, tol=1e-5, test=True):
+def ica(X, cycles=1000, tol=1e-5, test=False):
     X = whiten(center(X))
     nrows = X.shape[0]
     W = np.zeros((nrows, nrows))
@@ -68,58 +47,5 @@ def ica(X, cycles=1000, tol=1e-5, test=True):
             if distance < tol:
                 break
         W[i, :] = w
-    S = W @ X
-    return S, distances
-
-def plot_sources(X, S, S_predicted):
-    fig, ax = plt.subplots(nrows=2, ncols=2)
-
-    for x in X:
-        ax[0, 0].plot(x)
-        ax[0, 0].set_title('mixture signals')
-
-    for s in S:
-        ax[0, 1].plot(s)
-        ax[0, 1].set_title("original sources")
-
-    for S_predicted in S_predicted:
-        ax[1, 0].plot(S_predicted)
-        ax[1, 0].set_title("predicted sources")
-    fig.tight_layout()
-    plt.show()
-
-# def mix_sources(mixtures, apply_noise=False):
-#     for i in range(len(mixtures)):
-#         max_val = np.max(mixtures[i])
-#         if max_val > 1 or np.min(mixtures[i]) < 1:
-#             mixtures[i] = mixtures[i] / (max_val / 2) - 0.5
-#     X = np.c_[[mix for mix in mixtures]]
-#     if apply_noise:
-#         X += 0.02 * np.random.normal(size=X.shape)
-#     return X
-
-# def example():
-#     sampling_rate, mix1 = wavfile.read('mix1.wav')
-#     sampling_rate, mix2 = wavfile.read('mix2.wav')
-#     sampling_rate, source1 = wavfile.read('source1.wav')
-#     sampling_rate, source2 = wavfile.read('source2.wav')
-#     X = mix_sources([mix1, mix2])
-#     S = ica(X, cycles=1000)
-#     plot_mixture_sources_predictions(X, [source1, source2], S)
-#     wavfile.write('out1.wav', sampling_rate, S[0])
-#     wavfile.write('out2.wav', sampling_rate, S[1])
-
-# from sklearn.preprocessing import StandardScaler
-# X = StandardScaler().fit_transform(X)
-# print(X)
-# from sklearn.decomposition import PCA
-# pca = PCA(whiten=True)
-# pca.fit(D)
-# D = pca.transform(D)
-# print(D)
-
-if __name__ == '__main__':
-    np.random.seed(0)
-    X, S = load_data()
-    S_predicted, distances = ica(X, cycles=100)
-    plot_sources(X, S, S_predicted)
+    S_predicted = W @ X
+    return S_predicted, distances
